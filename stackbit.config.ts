ea2838f510fc7cc22a37ec683f824c29b57ae355
Patrick Crowe <54455975+pcrowe22@users.noreply.github.com>
2025-04-13 16:46:39 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,45 +1,28 @@
+// stackbit.config.ts
 import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
-  ssgName: "custom",
-  nodeVersion: "18",
-  devCommand: "node_modules/.bin/astro dev --port {PORT} --hostname 127.0.0.1",
-  experimental: {
-    ssg: {
-      name: "Astro",
-      logPatterns: {
-        up: ["is ready", "astro"],
-      },
-      directRoutes: {
-        "socket.io": "socket.io",
-      },
-      passthrough: ["/vite-hmr/**"],
-    },
-  },
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
-      contentDirs: ["src/content"],
+      contentDirs: ["content"],
       models: [
-	  {
-          name: "post",
+        {
+          name: "Page",
           type: "page",
-          urlPath: "/test/{slug}",
-          filePath: "src/content/{slug}.md",
-          fields: [{ name: "title", type: "string", required: true, default: "Post Title" },            
-		    { name: "pubDate", type: "date", required: true },
-            { name: "updatedDate", type: "date" },
-            { name: "description", type: "string" },
-            { name: "heroImage", type: "string" },],
-        },],
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
       assetsConfig: {
         referenceType: "static",
-        staticDir: "src/content",
-        uploadDir: "_images",
-        publicPath: "/src/content/",
-      },
-    }),
-  ],
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
+      }
+    })
+  ]
 });
